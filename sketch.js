53cc9473fac38829919ae7f2959a2b0d213d1518
Vania Sarawgi <vanusarawgi@gmail.com>
2020-08-29 13:04:17 +0530

    asynchronousBallMovement

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-var ball;
-
+var Ball;
+var database,position;
 function setup(){
+    database=firebase.database();
+    console.log(database);
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    Ball = createSprite(250,250,10,10);
+    Ball.shapeColor = "red";
+    BallPosition = database.ref('Ball/position');
+    BallPosition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
+    if(position!==undefined)
+    {
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -21,9 +27,31 @@ function draw(){
         changePosition(0,+1);
     }
     drawSprites();
+    }
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    Ball.x = ball.x + x;
+    Ball.y = ball.y + y;
+}
+
+function readPosition(data)
+{
+ position = data.val();
+ //console.log(position.x);
+ Ball.x = position.x;
+ Ball.y = position.y;
+}
+
+function showError()
+{
+    console.log("error in writing to the database");
+}
+
+function writePosition(x,y)
+{
+    database.ref('Ball/position').set({
+        'x': position.x+x,
+        'y': position.y+y   
+    })
 }
